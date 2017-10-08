7d4b7f48547a57490ae1b86edd22c5f7921f6ab2
jmschonfeld <jeremyschonfeld@gmail.com>
2017-10-07 22:12:03 -0400

    Finish all JUnits and comments for building block

--- src/main/java/sf/codingcomp/blocks/solution/BuildingBlockImpl.java
+++ src/main/java/sf/codingcomp/blocks/solution/BuildingBlockImpl.java
@@ -8,35 +8,77 @@ import sf.codingcomp.blocks.CircularReferenceException;
 public class BuildingBlockImpl implements BuildingBlock {
 	
 	/**
-	 * The building blocks above and below this building block
+	 * The building block above this building block
 	 */
-	private BuildingBlockImpl above, below;
+	private BuildingBlockImpl above;
+	
+	/**
+	 * The building block below this building block
+	 */
+	private BuildingBlockImpl below;
 
     @Override
     public Iterator<BuildingBlock> iterator() {
     		// Create a "ghost" starter block, so the first time next() is called, it will begin at this block
     		BuildingBlockImpl starter = new BuildingBlockImpl();
-    		starter.above = this;
+    		starter.above = this.getBottomOfStack();
     		
     		// Create and return the Iterator
         return new Iterator<BuildingBlock>() {
+        		/**
+        		 * Whether or not the current block has been removed (preventing double removes)
+        		 */
+        		boolean hasRemoved = false;
+        		
+        		/**
+        		 * The current block the iterator is pointing to
+        		 */
         		BuildingBlockImpl current = starter;
+        		
+        		/**
+        		 * The next block it will go to (or null)
+        		 */
+        		BuildingBlockImpl next = current.above;
 
 			@Override
 			public boolean hasNext() {
-				return current.above != null;
+				return next != null;
 			}
 
 			@Override
 			public BuildingBlock next() {
-				current = current.above;
+				current = next;
+				next = current.above;
+				
+				// Update the hasRemoved flag since we are pointing to a new block now
+				hasRemoved = false;
+				
 				return current;
-			}}
-        ;
+			}
+			
+			@Override
+			public void remove() {
+				// If next has not been called (current == starter) or the current block has already been removed
+				if (current.equals(starter) || hasRemoved) {
+					throw new IllegalStateException();
+				}
+				
+				// Update the above/below pointers of this block and its surrounding blocks
+				BuildingBlockImpl oldBelow = current.below, oldAbove = current.above;
+				oldBelow.above = oldAbove;
+				oldAbove.below = oldBelow;
+				current.above = null;
+				current.below = null;
+				
+				// Set the hasRemoved flag to true to prevent double removes
+				hasRemoved = true;
+			}
+        };
     }
 
     @Override
     public void stackOver(BuildingBlock b) {
+    		// Cast b to our implementation, since we know we will not be mixing implementations
         BuildingBlockImpl implementation = (BuildingBlockImpl)b;
         
     		if (b != null) {
@@ -49,6 +91,8 @@ public class BuildingBlockImpl implements BuildingBlock {
     			if (implementation.above != null) {
     				implementation.above.below = null;
     			}
+    			
+    			// Update the passed block's pointer
 			implementation.above = this;
     		}
     		
@@ -57,6 +101,7 @@ public class BuildingBlockImpl implements BuildingBlock {
 			this.below.above = null;
 		}
 		
+		// Update this block's pointer
 		this.below = implementation;
     }
 
@@ -75,6 +120,7 @@ public class BuildingBlockImpl implements BuildingBlock {
     				implementation.below.above = null;
     			}
     			
+    			// Update the passed block's pointer
 			implementation.below = this;
     		}
     		
@@ -83,6 +129,7 @@ public class BuildingBlockImpl implements BuildingBlock {
 			this.above.below = null;
 		}
 		
+		// Update this block's pointer
 		this.above = implementation;
     }
 
@@ -104,11 +151,14 @@ public class BuildingBlockImpl implements BuildingBlock {
     private boolean aboveLineContains(BuildingBlock b) {
     		BuildingBlockImpl current = above;
     		while (current != null) {
+    			// If we find block b, return immediately
     			if (current.equals(b)) {
     				return true;
     			}
     			current = current.above;
     		}
+    		
+    		// If we reach the top of the stack, return false
     		return false;
     }
     
@@ -120,12 +170,28 @@ public class BuildingBlockImpl implements BuildingBlock {
     private boolean belowLineContains(BuildingBlock b) {
 		BuildingBlockImpl current = below;
 		while (current != null) {
+			// If we find block b, return immediately
 			if (current.equals(b)) {
 				return true;
 			}
 			current = current.below;
 		}
+		
+		// If we reach the bottom of the stack, return false
 		return false;
-}
+    }
+    
+    /**
+     * Finds the bottom of the stack from this block
+     * @return The bottom of the stack
+     */
+    private BuildingBlockImpl getBottomOfStack() {
+		BuildingBlockImpl current = this;
+		while (current.below != null) {
+			// Iterate until there is no block below the current block
+			current = current.below;
+		}
+		return current;
+    }
 
 }
