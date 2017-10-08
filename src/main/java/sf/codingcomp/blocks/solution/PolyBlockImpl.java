8e185fb63b5b1f5116f4353f875fcc8df9a2f001
Eduardo Lira <eduardo.lira@cnhind.com>
2017-10-07 23:16:02 -0500

    More passings

--- src/main/java/sf/codingcomp/blocks/solution/PolyBlockImpl.java
+++ src/main/java/sf/codingcomp/blocks/solution/PolyBlockImpl.java
@@ -1,18 +1,15 @@
 package sf.codingcomp.blocks.solution;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
+import sf.codingcomp.blocks.BuildingBlock;
 import sf.codingcomp.blocks.PolyBlock;
 
 public class PolyBlockImpl implements PolyBlock {
-    private List<PolyBlock> connections = new ArrayList<>();
+    private List<PolyBlock> directConnections = new ArrayList<>();
     @Override
     public Iterator<PolyBlock> iterator() {
-        // TODO Auto-generated method stub
-        return null;
+        return new PolyBlockIterator(this);
     }
 
     @Override
@@ -20,7 +17,7 @@ public class PolyBlockImpl implements PolyBlock {
         if(aPolyBlock==null) return;
         if(Objects.equals(this, aPolyBlock)) return;
         if(this.contains(aPolyBlock)) return;
-        this.connections.add(aPolyBlock);
+        this.directConnections.add(aPolyBlock);
         aPolyBlock.connect(this);
     }
 
@@ -28,29 +25,81 @@ public class PolyBlockImpl implements PolyBlock {
     public void disconnect(PolyBlock aPolyBlock) {
         if(aPolyBlock==null) return;
         if(!this.contains(aPolyBlock)) return;
-        this.connections.remove(aPolyBlock);
+        this.directConnections.remove(aPolyBlock);
         aPolyBlock.disconnect(this);
     }
 
     @Override
     public boolean contains(PolyBlock aPolyBlock) {
-        return connections.contains(aPolyBlock);
+        return directConnections.contains(aPolyBlock);
     }
 
     @Override
     public int connections() {
-        return connections.size();
+        return directConnections.size();
     }
 
     @Override
     public int size() {
-        return 1+connections.size();
+    return directConnections.size()+1;
     }
 
     @Override
     public PolyBlock copy() {
         // TODO Auto-generated method stub
         return null;
+    }
+
+    @Override
+    public List<PolyBlock> getDirectConnections() {
+        return directConnections;
+    }
+
+    public class PolyBlockIterator implements Iterator<PolyBlock> {
+        private PolyBlock current;
+        private HashSet<PolyBlock> visitedPolyBlocks = new HashSet<>();
+        private Stack<PolyBlock> polyStack = new Stack<>();
+        private int localItr = 0;
+
+        public PolyBlockIterator(PolyBlock current) {
+            this.current = current;
+            visitedPolyBlocks.add(current);
+            polyStack.push(current);
+        }
+
+        @Override
+        public boolean hasNext() {
+            return current!=null;
+        }
+
+        @Override
+        public PolyBlock next() {
+            PolyBlock temp = current;
+            this.current = nextImpl();
+            return temp;
+        }
+
+        private PolyBlock nextImpl(){
+            while(polyStack.size()>0){
+                PolyBlock topBlock = polyStack.peek();
+                List<PolyBlock> children = topBlock.getDirectConnections();
+                while (localItr<children.size()){
+                    PolyBlock interested = children.get(localItr);
+                    if(visitedPolyBlocks.contains(interested)) {
+                        localItr++;
+                        continue;
+                    }
+                    polyStack.push(interested);
+                    visitedPolyBlocks.add(interested);
+                    localItr++;
+                    return interested;
+                }
+                polyStack.pop();
+                localItr = 0;
+            }
+            return null;
+        }
+
     }
 
 }
