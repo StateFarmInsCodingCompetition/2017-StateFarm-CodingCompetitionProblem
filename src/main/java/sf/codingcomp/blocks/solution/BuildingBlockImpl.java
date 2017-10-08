494b4e3110125e87d584965ad8b0f50e425e8b9b
Junweiren <junweir@gmail.com>
2017-10-07 21:53:45 -0400

    first step

--- src/main/java/sf/codingcomp/blocks/solution/BuildingBlockImpl.java
+++ src/main/java/sf/codingcomp/blocks/solution/BuildingBlockImpl.java
@@ -1,11 +1,14 @@
 package sf.codingcomp.blocks.solution;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 
 import sf.codingcomp.blocks.BuildingBlock;
 
 public class BuildingBlockImpl implements BuildingBlock {
 
+    BuildingBlock blockAbove;
+    BuildingBlock blockUnder;
 
     @Override
     public Iterator<BuildingBlock> iterator() {
@@ -15,26 +18,34 @@ public class BuildingBlockImpl implements BuildingBlock {
 
     @Override
     public void stackOver(BuildingBlock b) {
+        if(findBlockUnder() == null || !findBlockUnder().equals(b)) {
+            blockUnder = b;
+            b.stackUnder(this);
+        }
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void stackUnder(BuildingBlock b) {
+        if(findBlockOver() == null || !findBlockOver().equals(b)) {
+            blockAbove = b;
+            b.stackOver(this);
+        }
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public BuildingBlock findBlockUnder() {
         // TODO Auto-generated method stub
-        return null;
+        return blockUnder;
     }
 
     @Override
     public BuildingBlock findBlockOver() {
         // TODO Auto-generated method stub
-        return null;
+        return blockAbove;
     }
 
 }
