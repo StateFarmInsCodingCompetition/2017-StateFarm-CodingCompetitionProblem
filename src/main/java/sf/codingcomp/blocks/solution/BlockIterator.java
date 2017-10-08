16619c0f3734ff68a3a95153fb5b06ed6963423a
dmarnol2 <dmarnol2@asu.edu>
2017-10-08 00:47:15 -0400

    hasNext ready, built iterator

--- src/main/java/sf/codingcomp/blocks/solution/BlockIterator.java
+++ src/main/java/sf/codingcomp/blocks/solution/BlockIterator.java
@@ -9,20 +9,27 @@ import java.util.Iterator;
 public class BlockIterator<BuildingBlock> implements Iterator<BuildingBlock>{
 	
 	
+	ArrayList<BuildingBlock> stacked;
+	Iterator check;
+	
 	public BlockIterator(ArrayList<BuildingBlock> block) {
 		
-		ArrayList<BuildingBlock> stacked = new ArrayList<BuildingBlock>(block);
-		Iterator check = stacked.iterator();
+		stacked = new ArrayList<BuildingBlock>(block);
+		check = stacked.iterator();
 	}
 	
 	@Override
 	public boolean hasNext() {
+		if (check.hasNext())
 		return true;
+		else
+			return false;
 				
 	}
 
 	@Override
 	public BuildingBlock next() {
+		
 		// TODO Auto-generated method stub
 		return null;
 	}
