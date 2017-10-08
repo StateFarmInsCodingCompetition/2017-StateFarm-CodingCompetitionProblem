633528cf3e2fb52ae3704bcc733a36b977b9337c
Amir <amir@aaomidi.com>
2017-10-08 00:48:26 -0400

    Finish submission.

--- src/main/java/sf/codingcomp/blocks/solution/PolyBlockImpl.java
+++ src/main/java/sf/codingcomp/blocks/solution/PolyBlockImpl.java
@@ -1,51 +1,157 @@
 package sf.codingcomp.blocks.solution;
 
-import java.util.Iterator;
-
 import sf.codingcomp.blocks.PolyBlock;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class PolyBlockImpl implements PolyBlock {
 
-    @Override
-    public Iterator<PolyBlock> iterator() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+    protected Set<PolyBlock> connections = new HashSet<>();
+
+    private AtomicBoolean lock = new AtomicBoolean();
 
     @Override
     public void connect(PolyBlock aPolyBlock) {
-        // TODO Auto-generated method stub
+        if (aPolyBlock == null || aPolyBlock == this) {
+            return;
+        }
 
+        if (lock.get()) {
+            return;
+        }
+
+        this.connections.add(aPolyBlock);
+        lock.set(true);
+        aPolyBlock.connect(this);
+        lock.set(false);
     }
 
     @Override
     public void disconnect(PolyBlock aPolyBlock) {
-        // TODO Auto-generated method stub
+        if (aPolyBlock == null) {
+            return;
+        }
 
+        if (lock.get()) {
+            return;
+        }
+
+        this.connections.remove(aPolyBlock);
+        lock.set(true);
+        aPolyBlock.disconnect(this);
+        lock.set(false);
     }
 
     @Override
     public boolean contains(PolyBlock aPolyBlock) {
-        // TODO Auto-generated method stub
-        return false;
+        return aPolyBlock != this && connections.contains(aPolyBlock);
     }
 
     @Override
     public int connections() {
-        // TODO Auto-generated method stub
-        return 0;
+        return connections.size();
     }
 
     @Override
     public int size() {
-        // TODO Auto-generated method stub
-        return 0;
+        int i = 0;
+        for (PolyBlock polyBlock : this) {
+            i++;
+        }
+        return i;
     }
 
     @Override
     public PolyBlock copy() {
-        // TODO Auto-generated method stub
-        return null;
+        Map<PolyBlock, PolyBlock> copies = new HashMap<>();
+
+        iterator().forEachRemaining(block -> copies.put(block, new PolyBlockImpl()));
+
+        copies.keySet().forEach(block -> {
+            for (PolyBlock inner : ((PolyBlockImpl) block).connections) {
+                copies.get(block).connect(copies.get(inner));
+            }
+        });
+
+        return copies.get(this);
     }
 
+    @Override
+    public Iterator<PolyBlock> iterator() {
+        return new PolyBlockIterator(this);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PolyBlockImpl that = (PolyBlockImpl) o;
+
+        return connections.size() == that.connections.size();
+    }
+
+
+    private class PolyBlockIterator implements Iterator<PolyBlock> {
+
+        private LinkedList<PolyBlock> stack = new LinkedList<>();
+        private PolyBlock current;
+
+        private PolyBlockIterator(PolyBlock pb) {
+            this.prepare(pb);
+        }
+
+        private void prepare(PolyBlock p) {
+            Set<PolyBlock> visited = new HashSet<>();
+            Stack<PolyBlock> stack = new Stack<>();
+
+            stack.push(p);
+
+            while (!stack.isEmpty()) {
+                PolyBlock pb = stack.pop();
+                visited.add(pb);
+                PolyBlockImpl polyBlock = (PolyBlockImpl) pb;
+                for (PolyBlock b : polyBlock.connections) {
+                    if (visited.contains(b)) {
+                        continue;
+                    }
+                    stack.push(b);
+                }
+            }
+
+            this.stack.addAll(visited);
+        }
+
+        @Override
+        public boolean hasNext() {
+            return !stack.isEmpty();
+        }
+
+        @Override
+        public PolyBlock next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+
+            current = stack.pop();
+
+            return current;
+        }
+
+        @Override
+        public void remove() {
+            PolyBlock pb = current;
+            if (pb == null) {
+                throw new NoSuchElementException();
+            }
+            PolyBlockImpl pbi = (PolyBlockImpl) pb;
+
+            for (PolyBlock b : pbi.connections) {
+                if (b == pbi) {
+                    b.disconnect(pb);
+                }
+            }
+        }
+    }
 }
