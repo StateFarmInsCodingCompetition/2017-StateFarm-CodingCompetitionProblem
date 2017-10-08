13cf2212427aed92163082add84b8e62801a9867
Logan Seaburg <logan.seaburg@gmail.com>
2017-10-07 19:37:05 -0700

    Added stuff

--- src/main/java/sf/codingcomp/blocks/solution/BuildingBlockImpl.java
+++ src/main/java/sf/codingcomp/blocks/solution/BuildingBlockImpl.java
@@ -21,23 +21,6 @@ public class BuildingBlockImpl implements BuildingBlock {
     		if (this.findBlockUnder() == b) {
 			return;
 		}
-//	    	if (b == null) {
-//			if (this.findBlockUnder() != null) {
-//				this.below.stackUnder(null);
-//			}
-//			below = null;
-//			above = null;
-//		} else {
-//	    		if (below == null) {
-//				below = b;
-//		        b.stackOver(this);
-//			} else {
-//				below.stackOver(null);
-//				below = b;
-//		        b.stackOver(this);
-//			}
-//		}
-    	
     		below = b;
     		if (b != null) {
     			below.stackUnder(this);
@@ -50,25 +33,13 @@ public class BuildingBlockImpl implements BuildingBlock {
     		if (this.findBlockOver() == b) {
     			return;
     		}
-//	    	if (b == null) {
-//				above = null;
-//				below = null;
-//		} else {
-//	    		if (above == null) {
-//	    			this.above = b;
-//	    	        b.stackOver(this);
-//	    		} else {
-//	    			this.above.stackOver(null);
-//	    			this.above = b;
-//	    	        b.stackOver(this);
-//	    		}
-//		}
     		if (b == null) {
+    			above.stackOver(null);
     			above = null;
     			return;
     		}
-    		if (above != null && above.findBlockUnder() != null) {
-    			above.findBlockUnder().stackUnder(null);
+    		if (b.findBlockUnder() != null) {
+    			b.findBlockUnder().stackUnder(null);
     		}
     		above = b;
     		if (b != null) {
