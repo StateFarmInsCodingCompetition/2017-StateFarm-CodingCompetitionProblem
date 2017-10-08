b43b8fb44994704bbfb4232791c00f440eba921e
karshinlin <lin.karshin@gmail.com>
2017-10-07 22:54:01 -0400

    PolyBlock implementation.

--- src/main/java/sf/codingcomp/blocks/solution/PolyBlockImpl.java
+++ src/main/java/sf/codingcomp/blocks/solution/PolyBlockImpl.java
@@ -1,51 +1,109 @@
 package sf.codingcomp.blocks.solution;
 
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Stack;
 
+import sf.codingcomp.blocks.CircularReferenceException;
 import sf.codingcomp.blocks.PolyBlock;
 
 public class PolyBlockImpl implements PolyBlock {
-
+	
+	HashSet<PolyBlock> connectionSet = new HashSet<>();
+	
     @Override
-    public Iterator<PolyBlock> iterator() {
-        // TODO Auto-generated method stub
-        return null;
+    public Iterator<PolyBlock> iterator() {   	
+	    	Iterator<PolyBlock> it = new Iterator<PolyBlock>() {
+	            private int currentIndex = 0;
+	            	Iterator<PolyBlock> connectionIter = connectionSet.iterator();
+	            	private HashSet<PolyBlock> visited = new HashSet<>();
+		        private Stack<PolyBlock> traversed = new Stack<>();
+		        PolyBlockImpl.this.getClass().sizeHelper(visited, PolyStackImpl.this, traversed);
+		        
+	            @Override
+	            public boolean hasNext() {
+	            		if (currentIndex == 0) return true;
+	            		else {
+	            			return connectionIter.hasNext();
+	            		}
+	            }
+	
+	            @Override
+	            public PolyBlock next() {
+	            		if (currentIndex == 0) {
+	            			currentIndex++;
+	            			return PolyBlockImpl.this;
+	            		} else {
+	            			currentIndex++;
+	            			return connectionIter.next();
+	            		}
+	            }
+	
+	            @Override
+	            public void remove() {
+	                throw new UnsupportedOperationException();
+	            }
+	    	};    
+	    return it;
     }
 
     @Override
     public void connect(PolyBlock aPolyBlock) {
-        // TODO Auto-generated method stub
-
+        if (aPolyBlock != null && aPolyBlock != this && !connectionSet.contains(aPolyBlock)) {
+        		connectionSet.add(aPolyBlock);
+        		((PolyBlockImpl) aPolyBlock).connectionSet.add(this);
+        } 
+//        else if (aPolyBlock == this) {
+//        		throw new CircularReferenceException("Attempt to connect block to itself");
+//        }
     }
 
     @Override
     public void disconnect(PolyBlock aPolyBlock) {
-        // TODO Auto-generated method stub
-
+        if (aPolyBlock != null && connectionSet.contains(aPolyBlock)) {
+        		connectionSet.remove(aPolyBlock);
+        		((PolyBlockImpl) aPolyBlock).connectionSet.remove(this);
+        }
     }
 
     @Override
     public boolean contains(PolyBlock aPolyBlock) {
-        // TODO Auto-generated method stub
-        return false;
+        return connectionSet.contains(aPolyBlock);
     }
 
     @Override
     public int connections() {
-        // TODO Auto-generated method stub
-        return 0;
+        return connectionSet.size();
     }
 
     @Override
     public int size() {
-        // TODO Auto-generated method stub
-        return 0;
+    		HashSet<PolyBlock> visited = new HashSet<>();
+    		//Integer sizeCounter = new Integer(0);
+    		Stack<PolyBlock> traversed = new Stack<>();
+    		sizeHelper(visited, this, traversed);
+        return traversed.size();
+    }
+    public void sizeHelper(HashSet<PolyBlock> visited, PolyBlock current, Stack<PolyBlock> traversed) {
+    		if (!visited.contains(current)) {
+    			traversed.push(current);
+    			visited.add(current);
+    			for (PolyBlock aBlock : ((PolyBlockImpl) current).connectionSet) {
+    				if (!visited.contains(aBlock)) {
+    					sizeHelper(visited, aBlock, traversed);
+    				}
+    			}
+    		}
     }
 
     @Override
     public PolyBlock copy() {
-        // TODO Auto-generated method stub
-        return null;
+//        PolyBlockImpl temp = new PolyBlockImpl();
+//        for (PolyBlock conn : connectionSet) {
+//        		temp.connectionSet.add(conn);
+//        }
+//        return temp;
+    		return this;
     }
 
 }
