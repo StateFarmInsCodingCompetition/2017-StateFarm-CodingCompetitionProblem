da81a86fe387c294ea1b30343a1c326736333c48
Cyclicduck <caleb.ji@wustl.edu>
2017-10-07 21:39:52 -0500

    awer

--- src/main/java/sf/codingcomp/blocks/solution/PolyBlockImpl.java
+++ src/main/java/sf/codingcomp/blocks/solution/PolyBlockImpl.java
@@ -1,42 +1,65 @@
 package sf.codingcomp.blocks.solution;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Stack;
 
 import sf.codingcomp.blocks.PolyBlock;
 
 public class PolyBlockImpl implements PolyBlock {   
 	public ArrayList<PolyBlock> connected = new ArrayList<PolyBlock>();
+	public int sizeOfConnections;
     @Override 
     public Iterator<PolyBlock> iterator() { // bfs  
         // TODO Auto-generated method stub
     	
-        return null;
+    	Iterator<PolyBlock> itr = new Iterator<PolyBlock>(){
+
+			@Override
+			public boolean hasNext() {
+				// TODO Auto-generated method stub
+				return false;
+			}
+
+			@Override
+			public PolyBlock next() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+    		
+    	};
+    	
+        return itr;
     }
   
 
     @Override
     public void connect(PolyBlock aPolyBlock) {
         // TODO Auto-generated method stub
-    	this.connected.add(aPolyBlock);
-    	((PolyBlockImpl) aPolyBlock).connected.add(this);
+    	if(!this.equals(aPolyBlock) && aPolyBlock != null){
+    		this.connected.add(aPolyBlock);
+        	((PolyBlockImpl) aPolyBlock).connected.add(this);
+    	}	
     }
 
     @Override
     public void disconnect(PolyBlock aPolyBlock) {
         // TODO Auto-generated method stub
-    	Iterator itr = this.connected.iterator();
-    	while(itr.hasNext()){
-    		if(itr.next().equals(aPolyBlock)){
-    			itr.remove();
-    		}
-    	}
-    	
-    	Iterator itr1 = ((PolyBlockImpl) aPolyBlock).connected.iterator();
-    	while(itr1.hasNext()){
-    		if(itr1.next().equals(this)){
-    			itr1.remove();
-    		}
+    	if(aPolyBlock != null){
+	    	Iterator itr = this.connected.iterator();
+	    	while(itr.hasNext()){
+	    		if(itr.next().equals(aPolyBlock)){
+	    			itr.remove();
+	    		}
+	    	}
+	    	
+	    	Iterator itr1 = ((PolyBlockImpl) aPolyBlock).connected.iterator();
+	    	while(itr1.hasNext()){
+	    		if(itr1.next().equals(this)){
+	    			itr1.remove();
+	    		}
+	    	}
     	}
     }
 
@@ -61,7 +84,35 @@ public class PolyBlockImpl implements PolyBlock {
     @Override
     public int size() {
         // TODO Auto-generated method stub
-        return 0;
+    	HashSet<PolyBlock> hs = new HashSet<PolyBlock>();
+    	hs.add(this);
+    	Stack<PolyBlock> st = new Stack<PolyBlock>();
+    	for(PolyBlock pb: this.connected){
+    		st.push(pb);  		
+    	}
+    	while(st.size() > 0){
+			PolyBlock pb = st.pop();
+			Iterator it = hs.iterator();
+			boolean alreadyFound = false;
+			while(it.hasNext()){
+				if(it.next().equals(pb)){
+					alreadyFound = true;
+					break;
+				}
+			}
+			
+			if(!alreadyFound){
+				hs.add(pb);
+				for(PolyBlock pb1: ((PolyBlockImpl) pb).connected){
+		    		st.push(pb1);  		
+		    	}
+			}
+		}
+        return hs.size();
+    }
+    
+    public void dfs(HashSet hs, PolyBlock pb){
+    	
     }
 
     @Override
