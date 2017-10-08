71693473cf708cd74ec0cec56d3d24d7b0b90147
rbrick <rbrickproductions123@gmail.com>
2017-10-07 23:46:47 -0500

    Implement BuildingBlockImpl

--- src/main/java/sf/codingcomp/blocks/solution/StorageBuildingBlockImpl.java
+++ src/main/java/sf/codingcomp/blocks/solution/StorageBuildingBlockImpl.java
@@ -3,17 +3,16 @@ package sf.codingcomp.blocks.solution;
 import sf.codingcomp.blocks.StorageBuildingBlock;
 
 public class StorageBuildingBlockImpl<T> extends BuildingBlockImpl implements StorageBuildingBlock<T> {
+    private T value;
 
     @Override
     public T getValue() {
-        // TODO Auto-generated method stub
-        return null;
+        return value;
     }
 
     @Override
     public void setValue(T value) {
-        // TODO Auto-generated method stub
-
+        this.value = value;
     }
 
 }
