22cedd80146ba70772136eee5f3aa423ffc8621f
CodingBash <bbecerr@outlook.com>
2017-10-07 23:50:51 -0500

    Quick implementation of generic types

--- src/main/java/sf/codingcomp/blocks/solution/StoragePolyBlockImpl.java
+++ src/main/java/sf/codingcomp/blocks/solution/StoragePolyBlockImpl.java
@@ -4,16 +4,16 @@ import sf.codingcomp.blocks.StoragePolyBlock;
 
 public class StoragePolyBlockImpl<T> extends PolyBlockImpl implements StoragePolyBlock<T> {
 
+	T value; 
+	
     @Override
     public T getValue() {
-        // TODO Auto-generated method stub
-        return null;
+        return value;
     }
 
     @Override
     public void setValue(T value) {
-        // TODO Auto-generated method stub
-
+       this.value = value;
     }
 
 }
