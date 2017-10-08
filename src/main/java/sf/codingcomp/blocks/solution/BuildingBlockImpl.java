810af299390571278052af7a680fbeb1c8c45028
Evan Borchert <eborchert6@gmail.com>
2017-10-07 21:49:09 -0400

    Added constructors and some methods.

--- src/main/java/sf/codingcomp/blocks/solution/BuildingBlockImpl.java
+++ src/main/java/sf/codingcomp/blocks/solution/BuildingBlockImpl.java
@@ -5,7 +5,14 @@ import java.util.Iterator;
 import sf.codingcomp.blocks.BuildingBlock;
 
 public class BuildingBlockImpl implements BuildingBlock {
-
+	
+	BuildingBlock above;
+	BuildingBlock below;
+	public BuildingBlockImpl(){
+		above = null;
+		below = null;
+		
+	}
     @Override
     public Iterator<BuildingBlock> iterator() {
         // TODO Auto-generated method stub
@@ -15,25 +22,27 @@ public class BuildingBlockImpl implements BuildingBlock {
     @Override
     public void stackOver(BuildingBlock b) {
         // TODO Auto-generated method stub
+    	below = b;
         
     }
 
     @Override
     public void stackUnder(BuildingBlock b) {
         // TODO Auto-generated method stub
+    	above = b;
         
     }
 
     @Override
     public BuildingBlock findBlockUnder() {
         // TODO Auto-generated method stub
-        return null;
+        return below;
     }
 
     @Override
     public BuildingBlock findBlockOver() {
         // TODO Auto-generated method stub
-        return null;
+        return above;
     }
 
 }
