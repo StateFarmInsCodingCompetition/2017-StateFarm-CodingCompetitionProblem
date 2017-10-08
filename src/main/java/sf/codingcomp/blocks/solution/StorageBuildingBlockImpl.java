2ba29feabbfd3238c677bdd052abca5ed8a8fba2
Karan Achtani <Karan.achtani@gatech.edu>
2017-10-07 22:45:28 -0400

    storage building block

--- src/main/java/sf/codingcomp/blocks/solution/StorageBuildingBlockImpl.java
+++ src/main/java/sf/codingcomp/blocks/solution/StorageBuildingBlockImpl.java
@@ -3,16 +3,19 @@ package sf.codingcomp.blocks.solution;
 import sf.codingcomp.blocks.StorageBuildingBlock;
 
 public class StorageBuildingBlockImpl<T> extends BuildingBlockImpl implements StorageBuildingBlock<T> {
-
+	
+	T value;
+	
     @Override
     public T getValue() {
         // TODO Auto-generated method stub
-        return null;
+        return value;
     }
 
     @Override
     public void setValue(T value) {
         // TODO Auto-generated method stub
+    		this.value = value;
 
     }
 
