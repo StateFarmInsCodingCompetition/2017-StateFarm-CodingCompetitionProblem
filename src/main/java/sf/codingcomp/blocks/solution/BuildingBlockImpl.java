7caa8c8ae0d4b2845b7884fb23312e2af12fdc18
jmschonfeld <jeremyschonfeld@gmail.com>
2017-10-07 21:37:01 -0400

    Basics of Building blocks (all but iterator)

--- src/main/java/sf/codingcomp/blocks/solution/BuildingBlockImpl.java
+++ src/main/java/sf/codingcomp/blocks/solution/BuildingBlockImpl.java
@@ -3,37 +3,129 @@ package sf.codingcomp.blocks.solution;
 import java.util.Iterator;
 
 import sf.codingcomp.blocks.BuildingBlock;
+import sf.codingcomp.blocks.CircularReferenceException;
 
 public class BuildingBlockImpl implements BuildingBlock {
+	
+	/**
+	 * The building blocks above and below this building block
+	 */
+	private BuildingBlockImpl above, below;
 
     @Override
     public Iterator<BuildingBlock> iterator() {
-        // TODO Auto-generated method stub
-        return null;
+    		// Create a "ghost" starter block, so the first time next() is called, it will begin at this block
+    		BuildingBlockImpl starter = new BuildingBlockImpl();
+    		starter.above = this;
+    		
+    		// Create and return the Iterator
+        return new Iterator<BuildingBlock>() {
+        		BuildingBlockImpl current = starter;
+
+			@Override
+			public boolean hasNext() {
+				return current.above != null;
+			}
+
+			@Override
+			public BuildingBlock next() {
+				current = current.above;
+				return current;
+			}}
+        ;
     }
 
     @Override
     public void stackOver(BuildingBlock b) {
-        // TODO Auto-generated method stub
+        BuildingBlockImpl implementation = (BuildingBlockImpl)b;
         
+    		if (b != null) {
+    			// If b already exists below this block in the stack, throw an exception
+    			if (this.aboveLineContains(b)) {
+    				throw new CircularReferenceException();
+    			}
+    			
+    			// Prevent triangles
+    			if (implementation.above != null) {
+    				implementation.above.below = null;
+    			}
+			implementation.above = this;
+    		}
+    		
+    		// Prevent triangles
+		if (this.below != null) {
+			this.below.above = null;
+		}
+		
+		this.below = implementation;
     }
 
     @Override
     public void stackUnder(BuildingBlock b) {
-        // TODO Auto-generated method stub
+        BuildingBlockImpl implementation = (BuildingBlockImpl)b;
         
+    		if (b != null) {
+    			// If b already exists below this block in the stack, throw an exception
+    			if (this.belowLineContains(b)) {
+    				throw new CircularReferenceException();
+    			}
+    			
+    			// Prevent triangles
+    			if (implementation.below != null) {
+    				implementation.below.above = null;
+    			}
+    			
+			implementation.below = this;
+    		}
+    		
+    		// Prevent triangles
+		if (this.above != null) {
+			this.above.below = null;
+		}
+		
+		this.above = implementation;
     }
 
     @Override
     public BuildingBlock findBlockUnder() {
-        // TODO Auto-generated method stub
-        return null;
+        return below;
     }
 
     @Override
     public BuildingBlock findBlockOver() {
-        // TODO Auto-generated method stub
-        return null;
+        return above;
     }
+    
+    /**
+     * Check to see whether block b already exists above this block
+     * @param b The block to check for
+     * @return Whether b exists above this block
+     */
+    private boolean aboveLineContains(BuildingBlock b) {
+    		BuildingBlockImpl current = above;
+    		while (current != null) {
+    			if (current.equals(b)) {
+    				return true;
+    			}
+    			current = current.above;
+    		}
+    		return false;
+    }
+    
+    /**
+     * Check to see whether block b already exists below this block
+     * @param b The block to check for
+     * @return Whether b exists below this block
+     */
+    private boolean belowLineContains(BuildingBlock b) {
+		BuildingBlockImpl current = below;
+		while (current != null) {
+			if (current.equals(b)) {
+				return true;
+			}
+			current = current.below;
+		}
+		return false;
+}
 
 }
