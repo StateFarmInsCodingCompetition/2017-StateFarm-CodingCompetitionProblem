e9e3ea05c1a1c0cd829f74e314419f5e11a6a05b
jfed8 <jadenfeddock@gmail.com>
2017-10-07 20:29:15 -0600

    New Tests

--- src/main/java/sf/codingcomp/blocks/solution/BuildingBlockImpl.java
+++ src/main/java/sf/codingcomp/blocks/solution/BuildingBlockImpl.java
@@ -21,21 +21,27 @@ public class BuildingBlockImpl implements BuildingBlock {
     		if (this.findBlockUnder() == b) {
 			return;
 		}
-	    	if (b == null) {
-			below = null;
-			above = null;
-		} else {
-	    		if (below == null) {
-				below = b;
-		        b.stackOver(this);
-			} else {
-				below.stackOver(null);
-				below = b;
-		        b.stackOver(this);
-			}
-		}
+//	    	if (b == null) {
+//			if (this.findBlockUnder() != null) {
+//				this.below.stackUnder(null);
+//			}
+//			below = null;
+//			above = null;
+//		} else {
+//	    		if (below == null) {
+//				below = b;
+//		        b.stackOver(this);
+//			} else {
+//				below.stackOver(null);
+//				below = b;
+//		        b.stackOver(this);
+//			}
+//		}
     	
-//    		below = b;
+    		below = b;
+    		if (b != null) {
+    			below.stackUnder(this);
+    		}
         
     }
 
@@ -44,21 +50,30 @@ public class BuildingBlockImpl implements BuildingBlock {
     		if (this.findBlockOver() == b) {
     			return;
     		}
-	    	if (b == null) {
-				above = null;
-				below = null;
-		} else {
-	    		if (above == null) {
-	    			this.above = b;
-	    	        b.stackOver(this);
-	    		} else {
-	    			this.above.stackOver(null);
-	    			this.above = b;
-	    	        b.stackOver(this);
-	    		}
-		}
-    	
-//    		above = b;
+//	    	if (b == null) {
+//				above = null;
+//				below = null;
+//		} else {
+//	    		if (above == null) {
+//	    			this.above = b;
+//	    	        b.stackOver(this);
+//	    		} else {
+//	    			this.above.stackOver(null);
+//	    			this.above = b;
+//	    	        b.stackOver(this);
+//	    		}
+//		}
+    		if (b == null) {
+    			above = null;
+    			return;
+    		}
+    		if (above != null && above.findBlockUnder() != null) {
+    			above.findBlockUnder().stackUnder(null);
+    		}
+    		above = b;
+    		if (b != null) {
+    			above.stackOver(this);
+    		}
     }
 
     @Override
