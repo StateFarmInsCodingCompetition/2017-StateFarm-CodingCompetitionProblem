1886de26f6815a1618fe4a58f6af3c3e8a2bb876
Ayushi Koppu <Ayushik@Ayushis-MacBook-Air.local>
2017-10-08 00:54:21 -0400

    Finished

--- src/main/java/sf/codingcomp/blocks/solution/PolyBlockImpl.java
+++ src/main/java/sf/codingcomp/blocks/solution/PolyBlockImpl.java
@@ -1,45 +1,113 @@
 package sf.codingcomp.blocks.solution;
 
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Map;
 
 import sf.codingcomp.blocks.PolyBlock;
 
 public class PolyBlockImpl implements PolyBlock {
-
+	Map<PolyBlockImpl, LinkedList<PolyBlockImpl>> connections = new HashMap<>();
+	
     @Override
     public Iterator<PolyBlock> iterator() {
-        // TODO Auto-generated method stub
-        return null;
+        return new PBIterator();
+    }
+    
+    private class PBIterator implements Iterator<PolyBlock> {
+    	PolyBlockImpl curr;
+    	
+    	public PBIterator() {
+    		curr = new PolyBlockImpl();
+    	}
+    	
+    	public boolean hasNext() {
+    		return connections.get(curr).size() != 0;
+    	}
+    	
+    	public PolyBlock next() {
+    		PolyBlockImpl c = curr;
+    		curr = connections.get(curr).getFirst();
+    		return c;
+    	}
     }
 
     @Override
     public void connect(PolyBlock aPolyBlock) {
-        // TODO Auto-generated method stub
-
+    	PolyBlockImpl ablock = (PolyBlockImpl) aPolyBlock;
+        if(this != ablock) {
+        	if(connections.containsKey(this)) {
+	        	if(ablock != null) {
+	        	LinkedList<PolyBlockImpl> temp = connections.get(this);
+	        	temp.add(ablock);
+	        	connections.put(this, temp);
+	        	}
+	        } else {
+	        	if(ablock != null) {
+	        		LinkedList<PolyBlockImpl> temp = new LinkedList<>();
+	        		temp.add(ablock);
+	            	connections.put(this, temp);
+	        	}
+	        }
+	        if (connections.containsKey(ablock)) {
+	        	if(ablock != null) {
+	        		LinkedList<PolyBlockImpl> temp = connections.get(ablock);
+	            	temp.add(this);
+	            	connections.put(ablock, temp);
+	        	}
+	        } else {
+	        	if(ablock != null) {
+	        		LinkedList<PolyBlockImpl> temp = new LinkedList<>();
+	            	temp.add(this);
+	            	connections.put(ablock, temp);
+	        	}
+	        }
+        }
     }
 
     @Override
     public void disconnect(PolyBlock aPolyBlock) {
-        // TODO Auto-generated method stub
-
+    	PolyBlockImpl ablock = (PolyBlockImpl) aPolyBlock;
+        if(connections.containsKey(this)) {
+        	LinkedList<PolyBlockImpl> temp = connections.get(this);
+        	temp.remove(ablock);
+        	connections.put(this, temp);
+        }
+        if (connections.containsKey(ablock)) {
+        	LinkedList<PolyBlockImpl> temp = connections.get(ablock);
+        	temp.remove(this);
+        	connections.put(ablock, temp);
+        }
     }
 
     @Override
     public boolean contains(PolyBlock aPolyBlock) {
-        // TODO Auto-generated method stub
+    	PolyBlockImpl ablock = (PolyBlockImpl) aPolyBlock;
+        if(connections.containsKey(ablock)) {
+        	return connections.get(ablock).contains(this);
+        } else if(connections.containsKey(this)){
+        	return connections.get(this).contains(ablock);
+        }
         return false;
     }
 
     @Override
     public int connections() {
-        // TODO Auto-generated method stub
-        return 0;
+    	if(!connections.containsKey(this)) {
+    		return 0;
+    	} else if(connections.get(this).isEmpty()) {
+    		return 0;
+    	}
+    	return connections.get(this).size();
     }
 
     @Override
     public int size() {
-        // TODO Auto-generated method stub
-        return 0;
+    	if(connections.size() == 0) {
+    		return 1;
+    	}
+        return connections.size();
     }
 
     @Override
