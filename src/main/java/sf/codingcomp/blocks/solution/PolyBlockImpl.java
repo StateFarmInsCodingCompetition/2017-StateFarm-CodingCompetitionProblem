e3794905b064e06cb72b2f8f463df558da9ca2ac
jfed8 <jadenfeddock@gmail.com>
2017-10-07 22:52:05 -0600

    Poly

--- src/main/java/sf/codingcomp/blocks/solution/PolyBlockImpl.java
+++ src/main/java/sf/codingcomp/blocks/solution/PolyBlockImpl.java
@@ -1,11 +1,14 @@
 package sf.codingcomp.blocks.solution;
 
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
 
 import sf.codingcomp.blocks.PolyBlock;
 
 public class PolyBlockImpl implements PolyBlock {
 
+	Set<PolyBlockImpl> connects = new HashSet<PolyBlockImpl>();
     @Override
     public Iterator<PolyBlock> iterator() {
         // TODO Auto-generated method stub
@@ -15,31 +18,42 @@ public class PolyBlockImpl implements PolyBlock {
     @Override
     public void connect(PolyBlock aPolyBlock) {
         // TODO Auto-generated method stub
-
+    		if (aPolyBlock == this) return;
+    		if (aPolyBlock == null) return;
+    		PolyBlockImpl pi = (PolyBlockImpl) aPolyBlock;
+    		connects.add(pi);
+    		pi.connects.add(this);
     }
 
     @Override
     public void disconnect(PolyBlock aPolyBlock) {
         // TODO Auto-generated method stub
-
+    		if (aPolyBlock == this) return;
+    		if (aPolyBlock == null) return;
+    		PolyBlockImpl pi = (PolyBlockImpl) aPolyBlock;
+    		connects.remove(pi);
+    		pi.connects.remove(this);
     }
 
     @Override
     public boolean contains(PolyBlock aPolyBlock) {
         // TODO Auto-generated method stub
+    		if (connects.contains(aPolyBlock)) {
+    			return true;
+    		}
         return false;
     }
 
     @Override
     public int connections() {
         // TODO Auto-generated method stub
-        return 0;
+        return connects.size();
     }
 
     @Override
     public int size() {
         // TODO Auto-generated method stub
-        return 0;
+        return 1;
     }
 
     @Override
