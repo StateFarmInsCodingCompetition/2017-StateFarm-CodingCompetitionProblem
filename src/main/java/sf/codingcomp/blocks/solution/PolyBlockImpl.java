dcc8cc5d750e3400ded69b1fa145072a6373017e
U-ACCOUNTS\sheil <ACCOUNTS+sheil@URB218-19.seasad.wustl.edu>
2017-10-07 23:00:00 -0500

    junits pass

--- src/main/java/sf/codingcomp/blocks/solution/PolyBlockImpl.java
+++ src/main/java/sf/codingcomp/blocks/solution/PolyBlockImpl.java
@@ -1,6 +1,7 @@
 package sf.codingcomp.blocks.solution;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Stack;
@@ -10,6 +11,8 @@ import sf.codingcomp.blocks.PolyBlock;
 public class PolyBlockImpl implements PolyBlock {   
 	public ArrayList<PolyBlock> connected = new ArrayList<PolyBlock>();
 	public int iterPos;
+	public boolean eqInProgress;
+	private HashSet<Long> visited;
     @Override 
     public Iterator<PolyBlock> iterator() { // bfs  
         // TODO Auto-generated method stub
@@ -43,7 +46,7 @@ public class PolyBlockImpl implements PolyBlock {
     			return;
     		}
     	}
-    	if(!this.equals(aPolyBlock) && aPolyBlock != null){
+    	if(!(this==aPolyBlock) && aPolyBlock != null){
     		this.connected.add(aPolyBlock);
         	((PolyBlockImpl) aPolyBlock).connected.add(this);
     	}	
@@ -55,13 +58,13 @@ public class PolyBlockImpl implements PolyBlock {
     	if(aPolyBlock != null){
 	    	Iterator itr = this.connected.iterator();
 	    	while(itr.hasNext()){
-	    		if(itr.next().equals(aPolyBlock)){
+	    		if(itr.next()==aPolyBlock){
 	    			itr.remove();
 	    		}
 	    	}	    	
 	    	Iterator itr1 = ((PolyBlockImpl) aPolyBlock).connected.iterator();
 	    	while(itr1.hasNext()){
-	    		if(itr1.next().equals(this)){
+	    		if(itr1.next()==this){
 	    			itr1.remove();
 	    		}
 	    	}
@@ -73,7 +76,7 @@ public class PolyBlockImpl implements PolyBlock {
         // TODO Auto-generated method stub
     	Iterator itr = this.connected.iterator();
     	while(itr.hasNext()){
-    		if(itr.next().equals(aPolyBlock)){
+    		if(itr.next()==(aPolyBlock)){
     			return true;
     		}
     	}
@@ -100,7 +103,7 @@ public class PolyBlockImpl implements PolyBlock {
 			Iterator it = hs.iterator();
 			boolean alreadyFound = false;
 			while(it.hasNext()){
-				if(it.next().equals(pb)){
+				if(it.next()==(pb)){
 					alreadyFound = true;
 					break;
 				}
@@ -119,15 +122,67 @@ public class PolyBlockImpl implements PolyBlock {
     public void dfs(HashSet hs, PolyBlock pb){
     	
     }
+    
+    public void copyrec(PolyBlockImpl pos, PolyBlockImpl target, HashMap<PolyBlockImpl, PolyBlockImpl> map) {
+    	for (int i=0; i<pos.connected.size(); ++i) {
+    		if (map.containsKey(pos.connected.get(i))) {
+    			target.connected.add((PolyBlock)(map.get(pos.connected.get(i))));
+    		} else {
+    			PolyBlockImpl pb = new PolyBlockImpl();
+    			target.connected.add(pb);
+    			map.put((PolyBlockImpl)pos.connected.get(i), pb);
+    			copyrec((PolyBlockImpl)pos.connected.get(i), pb, map);
+    		}
+    	}
+    }
 
     @Override
     public PolyBlock copy() {
+    	PolyBlockImpl pbi = new PolyBlockImpl();
+    	HashMap<PolyBlockImpl, PolyBlockImpl> map = new HashMap<PolyBlockImpl, PolyBlockImpl>();
+    	map.put(this, pbi);
+    	copyrec(this, pbi, map);
+    	return pbi;
         // TODO Auto-generated method stub
-    	PolyBlock copiedPolyBlock = new PolyBlockImpl(); 
+    /*	PolyBlockImpl copiedPolyBlock = new PolyBlockImpl();
+    	copiedPolyBlock.iterPos = iterPos;
+    	copiedPolyBlock.connected = new ArrayList<PolyBlock>();
+    	for (PolyBlock pb: connected) {
+    		copiedPolyBlock.connected.add(pb.copy());
+    	}
     	
-    	((PolyBlockImpl) copiedPolyBlock).connected = (ArrayList<PolyBlock>) this.connected.clone();
+    	//((PolyBlockImpl) copiedPolyBlock).connected = (ArrayList<PolyBlock>) this.connected.clone();
     	
         return copiedPolyBlock;
+    */}
+    private boolean testeq(PolyBlockImpl a, PolyBlockImpl b) {
+    	long x = (1L<<32)*a.hashCode()+b.hashCode();
+    	if (visited.contains(x)) {
+    		return true;
+    	}
+    	visited.add(x);
+    	if (a.connected.size() != b.connected.size()) {
+    		return false;
+    	}
+    	for (int i=0; i<a.connected.size(); ++i) {
+    		if (!testeq((PolyBlockImpl)a.connected.get(i), (PolyBlockImpl)b.connected.get(i))) {
+    			return false;
+    		}
+    	}
+    	return true;
     }
-
+    
+    public boolean equals(Object other) {
+    	visited = new HashSet<Long>();
+    	if (connected.size() != ((PolyBlockImpl)other).connected.size()) {
+    		return false;
+    	}
+    	for (int i=0; i<connected.size(); ++i) {
+    		if (!testeq((PolyBlockImpl)(connected.get(i)), (PolyBlockImpl)(((PolyBlockImpl)other).connected.get(i)))) {
+    			return false;
+    		}
+    	}
+    	return true;
+    	//return this.connected.equals(((PolyBlockImpl)other).connected) && iterPos == ((PolyBlockImpl)other).iterPos;
+    }
 }
