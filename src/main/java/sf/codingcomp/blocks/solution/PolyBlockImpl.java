2522ebad970c8bf79992fb1858eb8cbac2867347
Eduardo Lira <eduardo.lira@cnhind.com>
2017-10-07 22:31:03 -0500

    Lets pass a couple poly block tests

--- src/main/java/sf/codingcomp/blocks/solution/PolyBlockImpl.java
+++ src/main/java/sf/codingcomp/blocks/solution/PolyBlockImpl.java
@@ -1,11 +1,14 @@
 package sf.codingcomp.blocks.solution;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
 
 import sf.codingcomp.blocks.PolyBlock;
 
 public class PolyBlockImpl implements PolyBlock {
-
+    private List<PolyBlock> connections = new ArrayList<>();
     @Override
     public Iterator<PolyBlock> iterator() {
         // TODO Auto-generated method stub
@@ -14,32 +17,34 @@ public class PolyBlockImpl implements PolyBlock {
 
     @Override
     public void connect(PolyBlock aPolyBlock) {
-        // TODO Auto-generated method stub
-
+        if(aPolyBlock==null) return;
+        if(Objects.equals(this, aPolyBlock)) return;
+        if(this.contains(aPolyBlock)) return;
+        this.connections.add(aPolyBlock);
+        aPolyBlock.connect(this);
     }
 
     @Override
     public void disconnect(PolyBlock aPolyBlock) {
-        // TODO Auto-generated method stub
-
+        if(aPolyBlock==null) return;
+        if(!this.contains(aPolyBlock)) return;
+        this.connections.remove(aPolyBlock);
+        aPolyBlock.disconnect(this);
     }
 
     @Override
     public boolean contains(PolyBlock aPolyBlock) {
-        // TODO Auto-generated method stub
-        return false;
+        return connections.contains(aPolyBlock);
     }
 
     @Override
     public int connections() {
-        // TODO Auto-generated method stub
-        return 0;
+        return connections.size();
     }
 
     @Override
     public int size() {
-        // TODO Auto-generated method stub
-        return 0;
+        return 1+connections.size();
     }
 
     @Override
