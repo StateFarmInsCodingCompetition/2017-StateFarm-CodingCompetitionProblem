596761e9b8348d0f6505e2fb6b11b7a48a989b23
jfed8 <jadenfeddock@gmail.com>
2017-10-07 19:25:56 -0600

    Stacks

--- src/main/java/sf/codingcomp/blocks/solution/BuildingBlockImpl.java
+++ src/main/java/sf/codingcomp/blocks/solution/BuildingBlockImpl.java
@@ -7,26 +7,39 @@ import sf.codingcomp.blocks.BuildingBlock;
 
 public class BuildingBlockImpl implements BuildingBlock {
 
-	BuildingBlock above;
-	BuildingBlock below;
+	BuildingBlock above = null;
+	BuildingBlock below = null;
 	
     @Override
     public Iterator<BuildingBlock> iterator() {
         // TODO Auto-generated method stub
 //        return findBlockOver().findBlockUnder();
-        return Arrays.<BuildingBlock>asList(above.findBlockUnder()).iterator();
+    		return this.iterator();
     }
 
     @Override
     public void stackOver(BuildingBlock b) {
-        this.above = b;
+    		if (above == null) {
+    			this.above = b;
+    	        b.stackUnder(this);
+    		} else {
+    			this.above.stackUnder(null);
+    			this.above = b;
+    	        b.stackUnder(this);
+    		}
         
     }
 
     @Override
     public void stackUnder(BuildingBlock b) {
-        this.below = b;
-        
+    		if (below == null) {
+			this.below = b;
+	        b.stackOver(this);
+		} else {
+			this.below.stackOver(null);
+			this.below = b;
+	        b.stackOver(this);
+		}
     }
 
     @Override
